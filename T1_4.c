e9f257c743682d0c0995bebc42597b4c88b0c4d1
Santhosh <sanvsam11@gmail.com>
2016-02-15 00:14:54 +0530

    very slow progress

---
+++ T1_4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ char *a[2]={"temp","3579"};
+ printf("%c",*(a[1]+1));
+ printf("%c",(*(a+1))[2]);
+ printf("%c\n",a[1][2]);
+ printf("%c,%c\n",*(a[3]+2),a[0][3]);
+return 0;
+}
