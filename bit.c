0d74a507f7ed31dfa3d492e474aea8e9b8d109b7
Santhosh <sanvsam11@gmail.com>
2016-03-02 08:13:35 +0530

    before ds

---
+++ bit.c
@@ -0,0 +1,7 @@
+//Program to perform bit operations
+#include<stdio.h>
+int main(){
+ int a;
+ printf("Enter a:");scanf("%d",&a);
+ printf("\nReversed all bits in a to get: %d",!a);
+}
--- xor1.c
+++ xor1.c
@@ -10,16 +10,19 @@ int main()
  return 0;
 } 
 */
-
+//wrong operator
 int bitCounter(int a){
 int x=1,count=0;
-for(int i=0;i<32;i++,x<<1)
- count=(!(a^x))?count+1:count;
+for(;x>0;x=x<<1)
+// printf("\nx:%d",x);
+ count=(a&x)?count+1:count;
 return count;}
 int main()
 {
 printf("Enter the number to find set bit count:");
 int a;scanf("%d",&a);
 printf("\n The count of set bits is:%d\n",bitCounter(a));
+//int x=1;x=x<<1;
+//printf("\n Left shifted 1 is:%d\n a&x is:%d\n after leftshift again:%d",a,a&x,(x<<1)&a);
 return 0;
 }
