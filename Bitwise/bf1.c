11f51d58f1bb3502dc9df01773067701719f0217
Santhosh Varathan <SanthoshVarathan@MacBook-Air.local>
2016-03-09 20:18:51 +0530

    tried and failed single line 1 count

--- Bitwise/bf1.c
+++ Bitwise/bf1.c
@@ -5,8 +5,15 @@
 
 int main()
 {
- int x;
+ int x,c=0;
  printf("Enter the number to count set bits:");
  scanf("%d",&x);
+//for(int i=1;i&(c=(i&x)?c:c++);i<<=1)printf("i=%d  c=%d\n",i,c);    //single line attempt
+//int i=1;while(i|(c-(c=(x&i)?c:++c))){i>>=1;printf("i=%d c=%d\n",i,c);}
+/* using elaborate code */
+for(int i=1;i;i<<=1){
+ if(!((i&x)==0))c++;
+}
 
+ printf("\nThe count is %d\n",c);
 }
